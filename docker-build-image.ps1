ea0d4522398c0337622d4deb47e04dbc74279529
b1203 <baolh.tgg@vnpt.vn>
2026-01-12 08:56:17 +0700

    no message

--- docker-build-image.ps1
+++ docker-build-image.ps1
@@ -6,4 +6,4 @@ docker build `
  --build-arg FRAPPE_PATH=https://github.com/frappe/frappe `
  --build-arg FRAPPE_BRANCH=v16.0.0-rc.1 `
  --build-arg APPS_JSON_BASE64=$APPS_JSON_BASE64 `
- --tag frappe:16rc1-20260106 .
+ --tag frappe:16rc1-20260111 .
